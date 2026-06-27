b3ce6eefbd81e7dbdc299d43b32284ed5a5795e6
aicompetition43 <aicompetition43@eceutil2.gpu.snuhpc>
2026-06-27 14:42:35 +0900

    modify show_center.py

--- show_center.py
+++ show_center.py
@@ -1,13 +1,23 @@
+import os
 from PIL import Image
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-def main():
-    filename = 'Close_10_13924'
+def show__center(filename : str):
+    #filename = 'Close_10_13924'
     fileinfo = filename.split('_')
-    img_filename = '../Dataset/1.competition_trainset/1_dataset/'+fileinfo[0]+'_CAPP_cam1_'+fileinfo[1]+'_'+fileinfo[2]+'.jpg'
-    image = Image.open(img_filename)
+    filename_list = [f'../Dataset/1.competition_trainset/1_dataset/'+fileinfo[0]+'_CAPP_cam'+str(i)+'_'+fileinfo[1]+'_'+fileinfo[2] for i in range(1, 6)]
+
+    # check if 5 image files exist
+    for filepath in filename_list:
+        if not (os.path.exists(filepath + '.txt') and os.path.exists(filepath + '.jpg')):
+            print(f'there is no {filepath}')
+            return None
+
+    # cam1 image
+    img_filepath = filename_list[0] + '.jpg'
+    image = Image.open(img_filepath)
     img_copy = np.array(image.convert('RGB')).copy()
 
     # test code...
@@ -32,6 +42,16 @@ def main():
 
     plt.imshow(img_copy)
     plt.show()
+
+def main():
+    with open('data/filename.txt') as f:
+        for line in f:
+            filename = '_'.join(line.strip().split())
+            show__center(filename)
+            
+            # wait until next input
+            input()
+
 
 if __name__ == '__main__':
     main()
