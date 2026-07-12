2a956d826c747b623e140ca08865f615a265d998
aicompetition43 <aicompetition43@eceutil1.gpu.snuhpc>
2026-07-12 16:22:46 +0900

    remove repeated paths

--- data/check_file.py
+++ data/check_file.py
@@ -1,13 +1,13 @@
 import os
 result = []
 
-with open('filepaths_bev.txt') as f:
+with open('filepaths_bev.txt', 'r') as f:
     for line in f:
-        imgpath = line.split(' ')[-1]
+        imgpath = line.strip().split(' ')[-1]
         
         if os.path.isfile(imgpath):
             result.append(line)
 
-with open('filepaths_bev.txt') as f:
+with open('filepaths_bev.txt', 'w') as f:
     for line in result:
         f.write(line)
